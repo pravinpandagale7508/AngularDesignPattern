97f17812ad6c0ea9c0ec7da6b15af0ade4982522
Pravin Pandagale <pravin.pandagalee@onesm.in>
2023-11-18 21:56:10 +0530

    Dependancy Injection

--- DesignPattern/src/app/poker-table/poker-table.component.ts
+++ DesignPattern/src/app/poker-table/poker-table.component.ts
@@ -14,4 +14,30 @@ export class PokerTableComponent {
   ngOnInit() {
     this.tables = this.tableService.getTables();
   }
+  snippet1:string = `export class GlobleConstants {
+    public POKER_TABLES=[
+    {name:"2 # Player",numSeats:2,bet:5,rake:3},
+    {name:"3 # Player",numSeats:3,bet:5,rake:3},
+    {name:"4 # Player",numSeats:4,bet:5,rake:3},
+    {name:"5 # Player",numSeats:5,bet:5,rake:3},
+    {name:"6 # Player",numSeats:6,bet:5,rake:3},]
+}`
+snippest2:string = `export class TableService {
+
+  constructor(private constants: GlobleConstants) { 
+
+  }
+  getTables(): any {   
+    return this.constants.POKER_TABLES;
+  }
+}
+`
+snippest3:string =`export class PokerTableComponent {
+  tables:any = [];
+  constructor(private tableService:TableService){
+    ...
+    ...
+    ...
+  }
+  }`
 }
